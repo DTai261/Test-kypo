a767f0103ce72fcb4fecfa32189c6fe9bf44b0e6
DTai261 <86485753+DTai261@users.noreply.github.com>
2023-08-10 22:48:16 +0700

    Update movies.sql

--- provisioning/roles/server/files/movies.sql
+++ provisioning/roles/server/files/movies.sql
@@ -1,5 +1,3 @@
----
-
 -- Sample data for movies table
 INSERT INTO movies (title, description, release_date, poster_url)
 VALUES ('Movie 1', 'Description of Movie 1', '2023-06-15', 'poster1.jpg'),
@@ -7,5 +5,3 @@ VALUES ('Movie 1', 'Description of Movie 1', '2023-06-15', 'poster1.jpg'),
        ('Movie 3', 'Description of Movie 3', '2023-07-01', 'poster3.jpg'),
        ('Movie 4', 'Description of Movie 4', '2023-07-10', 'poster4.jpg'),
        ('Movie 5', 'Description of Movie 5', '2023-07-15', 'poster5.jpg');
-
-...
